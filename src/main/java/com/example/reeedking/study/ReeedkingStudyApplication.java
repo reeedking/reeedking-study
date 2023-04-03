458bef5440a9596bb9a1326bd9df7db8080ab017
reeedking <121719158@qq.com>
2023-04-03 21:23:06 +0800

    初始化学习项目

---
+++ src/main/java/com/example/reeedking/study/ReeedkingStudyApplication.java
@@ -0,0 +1,13 @@
+package com.example.reeedking.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReeedkingStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReeedkingStudyApplication.class, args);
+    }
+
+}
